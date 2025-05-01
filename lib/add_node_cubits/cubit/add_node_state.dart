60542690015157bb3ad4ad8f24302df735c34492
Belal Karem <belalkarem41@gmail.com~>
2025-05-01 04:34:38 -0800

    add note state

--- lib/add_node_cubits/cubit/add_node_state.dart
+++ lib/add_node_cubits/cubit/add_node_state.dart
@@ -4,3 +4,13 @@ part of 'add_node_cubit.dart';
 sealed class AddNodeState {}
 
 final class AddNodeInitial extends AddNodeState {}
+
+final class AddNodeLoding extends AddNodeState {}
+
+final class AddNodeSuccess extends AddNodeState {}
+
+final class AddNodeFeliure extends AddNodeState {
+  final String errorMessage;
+
+  AddNodeFeliure(this.errorMessage);
+}
